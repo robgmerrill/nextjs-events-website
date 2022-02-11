4c0846f39eb2591c177ce6107826fce8bc008d3c
robgmerrill <robgmerrill@gmail.com>
2022-02-11 15:06:37 -0800

    Pages and routing

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <p>This is an app to find the latest DJ and other musical events.</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
