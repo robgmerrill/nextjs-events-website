d8fe365a148026c2b02461c5cbf62a36dfd3d441
robgmerrill <robgmerrill@gmail.com>
2022-02-11 16:04:27 -0800

    Create layout header and footer

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,12 @@
-import React from "react";
+import { useRouter } from "next/router";
+import Layout from "../components/Layout";
 
 export default function EventPage() {
+  const router = useRouter();
+  console.log(router);
   return (
-    <div>
+    <Layout>
       <h1>My Event</h1>
-    </div>
+    </Layout>
   );
 }
