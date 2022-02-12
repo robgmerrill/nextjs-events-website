7c3ff4702f44f842d1d309f9bcd781a2c9b4f07f
robgmerrill <robgmerrill@gmail.com>
2022-02-12 11:55:09 -0800

    Fetch data with getStaticProps

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,21 @@
 import Layout from "@/components/Layout";
+// import { API_URL } from "@/config/index";
 
 export default function EventsPage() {
+  // console.log({ events });
   return (
     <Layout>
       <h1>My Events</h1>
     </Layout>
   );
 }
+
+// export async function getServerSideProps() {
+//   const res = await fetch(`${API_URL}/api/events`);
+//   const events = await res.json();
+
+//   console.log(events);
+//   return {
+//     props: { events },
+//   };
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout title="DJ">
-      <meta name="description" content="Welcome to DJ Events" />
-
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    // 1 second delay if the data has changed
+    revalidate: 1,
+  };
 }
