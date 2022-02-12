c3ccf1b3fab94d70bc4d8d798ae78d4c787cc3e0
robgmerrill <robgmerrill@gmail.com>
2022-02-12 12:16:54 -0800

    Display events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,21 +1,33 @@
 import Layout from "@/components/Layout";
-// import { API_URL } from "@/config/index";
+import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
+import Link from "next/link";
 
-export default function EventsPage() {
-  // console.log({ events });
+export default function EventsPage({ events }) {
+  console.log(events);
   return (
-    <Layout>
-      <h1>My Events</h1>
+    <Layout title="DJ">
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
 
-// export async function getServerSideProps() {
-//   const res = await fetch(`${API_URL}/api/events`);
-//   const events = await res.json();
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
 
-//   console.log(events);
-//   return {
-//     props: { events },
-//   };
-// }
+  return {
+    props: { events: events.slice(0, 3) },
+    // 1 second delay if the data has changed
+    revalidate: 1,
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
 
 export default function HomePage({ events }) {
+  console.log(events);
   return (
     <Layout title="DJ">
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
 }
